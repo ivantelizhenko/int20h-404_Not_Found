4356b69ac70ab65dfd7cecddb2af5d21a9c83551
ivantelizhenko <telizenkoo@gmail.com>
2025-03-01 14:36:11 +0200

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AppLayout from "./components/AppLayout";
-import PageNotFound from "./pages/PageNotFound";
-
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<AppLayout />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>App</div>;
 }
 
 export default App;
