63e982f54969947594328331c2e838dc68a33375
ivantelizhenko <telizenkoo@gmail.com>
2025-03-01 23:00:07 +0200

    доробив взаємодію з AI

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,2 +1,2 @@
 export const CHATGPT_TOKEN =
-  "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";
+  "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";
--- src/services/getAnswer.ts
+++ src/services/getAnswer.ts
@@ -1,8 +1,11 @@
 import OpenAI from "openai";
 import { CHATGPT_TOKEN } from "./constants";
 
+const baseQuery =
+  "Створи мені на основі запиту, ось такий обєкт: { type: тут повинен бути тип приладу ,madeCompany: компанія, яка виробляє цей продукт,model: модель продукту,price: ціна,rating: рейтинг по пятибальній шкалі ,timeDeliveryDays: скільки днів доставик,numberOfReview: кількість відгуків,}. Якщо немає відповіді пиши null. І повертай мені тільки цей обєкт у форматі об'єкта json, а не рядка . Також потрібно щоб всі слова були англійською або null. Ось запит:";
+
 const openai = new OpenAI({
-  apiKey: CHATGPT_TOKEN, // Встав свій API-токен тут
+  apiKey: CHATGPT_TOKEN,
   dangerouslyAllowBrowser: true,
 });
 
@@ -15,14 +18,22 @@ export async function getAnswer(query: string) {
 
         {
           role: "user",
-          content: query,
+          content: baseQuery + " " + query,
         },
       ],
       store: true,
     });
 
-    if (!completion) throw new Error("Some problem with get data");
-    return completion.choices[0].message.content;
+    if (!completion.choices[0].message.content)
+      throw new Error("Some problem with get data");
+
+    const result = await JSON.parse(
+      completion.choices[0].message.content
+        .replaceAll("`", "")
+        .replaceAll("json", "")
+    );
+    // console.log(result);
+    return result;
   } catch (err) {
     console.error(err);
   }
